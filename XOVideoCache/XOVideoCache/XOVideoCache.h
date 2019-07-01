3c6292b0302bcccbe615ae3dc7707b4315bc6444
XO <xiangbolong@gmail.com>
2019-07-01 18:41:26 +0800

    init pr

---
+++ XOVideoCache/XOVideoCache/XOVideoCache.h
@@ -0,0 +1,19 @@
+//
+//  XOVideoCache.h
+//  XOVideoCache
+//
+//  Created by luo fengyuan on 2019/7/1.
+//  Copyright © 2019 luo fengyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XOVideoCache.
+FOUNDATION_EXPORT double XOVideoCacheVersionNumber;
+
+//! Project version string for XOVideoCache.
+FOUNDATION_EXPORT const unsigned char XOVideoCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XOVideoCache/PublicHeader.h>
+
+
---
+++ XOVideoCache/XOVideoCacheSampler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XOVideoCacheSampler
+//
+//  Created by luo fengyuan on 2019/7/1.
+//  Copyright © 2019 luo fengyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
