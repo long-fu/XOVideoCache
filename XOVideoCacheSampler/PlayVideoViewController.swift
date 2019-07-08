895bb28403c0e92bf24fdf70cced9143b43d18e6
XO <xiangbolong@gmail.com>
2019-07-08 16:56:57 +0800

    加入本地文件

---
+++ XOVideoCacheSampler/PlayVideoViewController.swift
@@ -0,0 +1,232 @@
+//
+//  PlayVideoViewController.swift
+//  XOVideoCacheSampler
+//
+//  Created by luo fengyuan on 2019/7/8.
+//  Copyright © 2019 luo fengyuan. All rights reserved.
+//
+
+//import XOVideoCache
+import UIKit
+import AVKit
+
+class PlayVideoViewController: UIViewController {
+    
+    private
+    var _player: AVPlayer? //播放器对象
+    private
+    var _playerItem: CachingPlayerItem? //播放资源对象
+    private
+    var _timeObserver: Any! //时间观察者
+    private
+    var _playerLayer: AVPlayerLayer?
+    
+    var resourceURL: URL!
+    
+//    var cacheFile: URL!
+//    var fileHandle: FileHandle?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let url = resourceURL else {
+            fatalError()
+        }
+
+        self._playerItem = CachingPlayerItem(url: url, customFileExtension: url.lastPathComponent)
+        self._playerItem?.delegate = self
+        self._playerItem?.download()
+        
+        self._player = AVPlayer(playerItem: self._playerItem)
+        self._playerLayer = AVPlayerLayer(player: self._player)
+        if #available(iOS 11.0, *) {
+            self._playerLayer?.frame = self.view.bounds.inset(by: self.view.safeAreaInsets)
+        } else {
+            // Fallback on earlier versions
+            self._playerLayer?.frame = self.view.bounds
+        }
+        self._playerLayer?.videoGravity = .resizeAspect //视频填充模式
+        
+        self.view.layer.addSublayer(self._playerLayer!)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self._player?.pause()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+#if false
+//    private
+    func __addPlayerItemObserver() {
+        // 为AVPlayerItem添加status属性观察，得到资源准备好，开始播放视频
+        _playerItem?.addObserver(self, forKeyPath: "status", options: .new, context: nil)
+        // 监听AVPlayerItem的loadedTimeRanges属性来监听缓冲进度更新
+        _playerItem?.addObserver(self, forKeyPath: "loadedTimeRanges", options: .new, context: nil)
+        NotificationCenter.default.addObserver(self,selector: #selector(__playerItemDidReachEnd(notification:)),name: .AVPlayerItemDidPlayToEndTime, object: _playerItem)
+    }
+    
+    //  通过KVO监控播放器状态
+    //
+    // - parameter keyPath: 监控属性
+    // - parameter object:  监视器
+    // - parameter change:  状态改变
+    // - parameter context: 上下文
+    override func observeValue(forKeyPath keyPath: String?,
+                               of object: Any?,
+                               change: [NSKeyValueChangeKey : Any]?,
+                               context: UnsafeMutableRawPointer?) {
+        guard let object = object as? AVPlayerItem  else { return }
+        guard let keyPath = keyPath else { return }
+
+        if keyPath == "status" {
+            if object.status == .readyToPlay { //当资源准备好播放，那么开始播放视频
+//                SVProgressHUD.dismiss()
+                _player?.play()
+//                _durationLabel.text = __formatPlayTime(seconds: CMTimeGetSeconds(object.duration))
+                self.__addProgressObserver()
+            } else if object.status == .failed || object.status == .unknown {
+                //                print("播放出错")
+                debugPrint("播放出错")
+//                SVProgressHUD.showError(withStatus: "播放失败")
+//                SVProgressHUD.dismiss(withDelay: 1.25)
+            }
+        } else if keyPath == "loadedTimeRanges" {
+            // 喊出进度
+            let loadedTime = __availableDurationWithplayerItem()
+            //            debugLog("当前加载进度\(loadedTime)")
+        }
+    }
+    
+    // 将秒转成时间字符串的方法，因为我们将得到秒。
+    private
+    func __formatPlayTime(seconds: Float64) -> String {
+        let min = Int(seconds / 60)
+        let sec = Int(seconds.truncatingRemainder(dividingBy: 60))
+        return String(format: "%02d:%02d", min, sec)
+    }
+    
+    
+    @objc
+    private func __playerItemDidReachEnd(notification: Notification) {
+        _player?.seek(to: CMTime.zero, toleranceBefore: CMTime.zero, toleranceAfter: CMTime.zero)
+//        _progress.progress = 0.0
+//        _playOrPauseButton.isSelected = true
+//        _playOrPauseButton1.setImage(#imageLiteral(resourceName: "video_play"), for: UIControl.State.normal)
+    }
+    
+    private
+    func __availableDurationWithplayerItem() -> TimeInterval {
+        guard let loadedTimeRanges = _player?.currentItem?.loadedTimeRanges,
+            let first = loadedTimeRanges.first else {
+                fatalError()
+        }
+        // 本次缓冲时间范围
+        let timeRange = first.timeRangeValue
+        let startSeconds = CMTimeGetSeconds(timeRange.start) // 本次缓冲起始时间
+        let durationSecound = CMTimeGetSeconds(timeRange.duration)// 缓冲时间
+        let result = startSeconds + durationSecound// 缓冲总长度
+        return result
+    }
+    
+    private
+    func __addProgressObserver(){
+        // 这里设置每秒执行一次.
+        _timeObserver = _player?.addPeriodicTimeObserver(forInterval: CMTimeMake(value: Int64(1.0),timescale: Int32(1.0)),queue: DispatchQueue.main) { [weak self] (time: CMTime) in
+            self?.__updateProgress(time)
+        }
+    }
+    private var _totalTime: Float64 = 0
+    private
+    func __updateProgress(_ time: CMTime) {
+        // CMTimeGetS econds函数是将CMTime转换为秒，如果CMTime无效，将返回NaN
+        guard let playerItem = _playerItem else {
+            return
+        }
+        let currentTime = CMTimeGetSeconds(time)
+        let totalTime = CMTimeGetSeconds(playerItem.duration)
+        // 更新显示的时间和进度条
+        //        debugLog("显示进度", totalTime)
+//        if totalTime > 6000 {
+//            self._progress.progress = 0
+//        } else {
+//            self._timeLabel.text = self.__formatPlayTime(seconds: CMTimeGetSeconds(time))
+//            let value = Float(currentTime/totalTime)
+//            //            debugLog("设置播放进度", currentTime, totalTime, value)
+//            if value == 0.0 {
+//                self._progress.progress = 0
+//            }else {
+//                self._progress.setProgress(Float(currentTime/totalTime), animated: true)
+//            }
+//
+//
+//        }
+        
+    }
+    
+    @objc private
+    func __playOrPauseButtonClicked(button: UIButton) {
+        if let player = _player {
+            if player.rate == 0 { // 点击时已暂停
+                //                button.setImage(UIImage(named:"pause"), for: .normal)
+//                _playOrPauseButton1.setImage(nil, for: UIControl.State.normal)
+//                _playOrPauseButton.isSelected = false
+                player.play()
+                
+            } else if player.rate == 1 {// 点击时正在播放
+                player.pause()
+//                _playOrPauseButton1.setImage(#imageLiteral(resourceName: "video_play"), for: UIControl.State.normal)
+//                _playOrPauseButton.isSelected = true
+            }
+        }
+    }
+    private
+    func __removeObserver() {
+        _playerItem?.removeObserver(self, forKeyPath: "status")
+        _playerItem?.removeObserver(self, forKeyPath: "loadedTimeRanges")
+        if let ob = _timeObserver {
+            _player?.removeTimeObserver(ob)
+        }
+        NotificationCenter.default.removeObserver(self,name: .AVPlayerItemDidPlayToEndTime,object: _playerItem)
+    }
+    
+    
+    private
+    func __didTapPreviewVideo() {
+        
+    }
+    
+    deinit {
+        __removeObserver()
+    }
+    #endif
+}
+
+extension PlayVideoViewController: CachingPlayerItemDelegate {
+    
+    func playerItemReadyToPlay(_ playerItem: CachingPlayerItem) {
+        debugPrint("开始播放")
+        _player?.play()
+    }
+    
+    func playerItem(_ playerItem: CachingPlayerItem, downloadingFailedWith error: Error) {
+        
+    }
+    
+    func playerItemPlaybackStalled(_ playerItem: CachingPlayerItem) {
+        
+    }
+    
+    func playerItem(_ playerItem: CachingPlayerItem, didFinishDownloadingData data: Data) {
+        debugPrint("视频下载完成", data.count)
+    }
+    
+    func playerItem(_ playerItem: CachingPlayerItem, didReceive data: Data, didDownloadBytesSoFar bytesDownloaded: Int, outOf bytesExpected: Int) {
+        
+    }
+    
+
+}
--- XOVideoCacheSampler/ViewController.swift
+++ XOVideoCacheSampler/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var video1Button: UIButton!
+    
+    @IBOutlet weak var video2Button: UIButton!
+    
+    @IBOutlet weak var video3Button: UIButton!
+    
+    @IBOutlet weak var video4Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        video1Button.addTarget(self, action: #selector(__buttonClicked(_:)), for: UIControl.Event.touchUpInside)
+        video2Button.addTarget(self, action: #selector(__buttonClicked(_:)), for: UIControl.Event.touchUpInside)
+        video3Button.addTarget(self, action: #selector(__buttonClicked(_:)), for: UIControl.Event.touchUpInside)
+        video4Button.addTarget(self, action: #selector(__buttonClicked(_:)), for: UIControl.Event.touchUpInside)
         // Do any additional setup after loading the view.
     }
 
+    @objc
+    private
+    func __buttonClicked(_ sender: UIButton?) {
+        guard let button = sender else {
+            return
+        }
+        let vc = PlayVideoViewController()
+        if button == video1Button {
+            vc.resourceURL = URL(string: "https://vodm0pihssv.vod.126.net/edu-video/nos/mp4/2017/4/2/1006075895_7ef266a2-85f6-4d8d-bb21-3b9332c342f4_sd.mp4")!
+        }
+        else if button == video2Button {
+            vc.resourceURL = URL(string: "https://apd-6da2d2ea49ade3248ca234fb8d8bae58.v.smtcdns.com/vweishi.tc.qq.com/AO5GtBLZz7QFEvE2LM01SnkfXoGNHfRBS_Uy95paEgY0/szg_10167773_40000_1b349e70032744309573336427a77bd6.f10.mp4?sdtfrom=v1103&guid=e737d5ae0915fdc9b2cc873c22147fef&vkey=BA1AB085D38353999F76C09D7E1A3AE11494A3694FCB04CEBBEF94E0A50EE10DD576086F64711AF2E718F5A407F41C0A56A78B9F1E1D2182E748C6B5B4186F0FE0F02F35923F3299B4877DF68A91E701721A7899108C828CD1FB7F47827240642E9506E6F836D9A5DB2A6D61DC23C6B47017D5BC020569350F8DE18ED4127FE7")!
+        }
+        else if button == video3Button {
+            vc.resourceURL = URL(string: "https://apd-2302da32dd361333d3766a61badbd4e3.v.smtcdns.com/vweishi.tc.qq.com/AEApKWP0Cpl8fnkapa4KoHQX1-su8wdFn8MFmBPlj5Eo/szg_42622107_40000_4d2c2090c980422c861e4f26fd68418f.f10.mp4?sdtfrom=v1103&guid=e737d5ae0915fdc9b2cc873c22147fef&vkey=2A84349C0817CEA02A679891BDE1CD06F733D2F58B24B39B9F253BC97F349AD054719B7581EA2D0F85421502E2A6B1434BD75952165665BBE95D9597319397A34F05D309C188D5A6E5906621133A698825AF0CABCB53F5178615268FF3344D838F74F67A739C97E8E5629E5F10B497CFD44D3952A0A0F059E6D373E834FF55A9")!
+        }
+        else if button == video4Button {
+            vc.resourceURL = URL(string: "https://apd-6da2d2ea49ade3248ca234fb8d8bae58.v.smtcdns.com/vweishi.tc.qq.com/A57dKsI6Cl4Wv3O5gqLxN_FEyvf3DUum9BE39J67W1ro/szg_59949277_40000_ea0881e4ef4e46cf8a056698c08c9d3e.f10.mp4?sdtfrom=v1103&guid=e737d5ae0915fdc9b2cc873c22147fef&vkey=E745B893184B40AF4DF892A4E92BF1562EBEA4689185FA16EFCA7601DAA24AB0B4B314202CE629DDB447600AAE7627F6FE1E121DCB0CDDA47FB080EE15695B4180321249CC8F52AB1C7B06AD8687409ABB37BE5A4A3D44239F675F9E88B725DADCB02A9BFCCB4917A098B5C8B936CB8B3B873677385723C24F679BB632C773D5")!
+        }
+        self.navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    
 
 }
 
